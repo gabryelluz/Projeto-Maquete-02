e348bf321d0e9f0132ca4b3ba6fee21bed6c50fc
gabryelluz <130998782+gabryelluz@users.noreply.github.com>
2023-11-03 00:13:40 -0300

    1

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const frontViewButton = document.getElementById('frontView');
 const topViewButton = document.getElementById('topView');
 
 frontViewButton.addEventListener('click', () => {
-  building.style.transform = 'rotateX(0deg) rotateY(0deg)';
+  building.style.transform = 'rotateX(20deg) rotateY(30deg)';
 });
 
 topViewButton.addEventListener('click', () => {
-  building.style.transform = 'rotateX(-60deg) rotateY(0deg)';
+  building.style.transform = 'rotateX(90deg) rotateY(0deg)';
 });
